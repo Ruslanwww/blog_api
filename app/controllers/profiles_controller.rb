1cfc9b61b0742cbd91ee27ca98db945c32e0b552
Ruslanwww <oruslano51@gmail.com>
2018-09-08 20:15:40 +0300

    Added user search

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,10 @@
+class ProfilesController < ApplicationController
+  def search_users
+    if params[:user]
+      @users = User.search(params[:user]).order("created_at DESC")
+      render json: @users
+    else
+      render status: :bad_request
+    end
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :confirmable, :validatable
+
+  def self.search(search)
+    where("username LIKE ?", "%#{search}%")
+  end
 end
